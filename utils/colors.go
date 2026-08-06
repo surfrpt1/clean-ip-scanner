5fff77cbf330e2221d2815d82f09a12d3407e1ea
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2026-08-06 07:18:44 +0330

    colors.go

--- utils/colors.go
+++ utils/colors.go
@@ -2,20 +2,42 @@ package utils
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/fatih/color"
 )
 
+const headerWidth = 49
+
+func CenterText(width int, text string) string {
+	pad := width - len(text)
+	if pad <= 0 {
+		return text
+	}
+	left := pad / 2
+	return strings.Repeat(" ", left) + text
+}
+
+func CenterInBox(width int, text string) string {
+	pad := width - len(text)
+	if pad <= 0 {
+		return text
+	}
+	left := pad / 2
+	right := pad - left
+	return strings.Repeat(" ", left) + text + strings.Repeat(" ", right)
+}
+
 func PrintHeader() {
 	cyan := color.New(color.FgCyan, color.Bold)
-	cyan.Println("=================================================")
-	cyan.Println("              CLEAN IP SCANNER")
-	cyan.Println("          Find the fastest clean IPs")
-	cyan.Println("=================================================")
+	cyan.Println(strings.Repeat("=", headerWidth))
+	cyan.Println(CenterText(headerWidth, "CLEAN IP SCANNER"))
+	cyan.Println(CenterText(headerWidth, "Find the fastest clean IPs"))
+	cyan.Println(strings.Repeat("=", headerWidth))
 }
 
 func PrintDesigner() {
 	green := color.New(color.FgGreen, color.Bold)
-	green.Println("...:..::.::: Engineered by: Anonymous :::.::..:...")
+	green.Println("...:..::.:::Engineered by: Anonymous:::.::..:...")
 	fmt.Println()
 }
